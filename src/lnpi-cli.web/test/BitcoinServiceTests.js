a93bb9e42bb38865d052fff22a550b7dd9dd5c2e
Sean O'Brien <seanmobrien@hotmail.com>
2019-09-07 17:41:08 -0500

    adding support for unit testing via mocha and chai

---
+++ src/lnpi-cli.web/test/BitcoinServiceTests.js
@@ -0,0 +1,17 @@
+﻿var assert = require('assert');
+var expect = require('chai').expect;
+var should = require('chai').should();
+
+var targetLibrary = require('./../services/bitcoin');
+
+it('getblockchaininfo should return chain data', function (done) {
+  var theCall = targetLibrary.getClientInstance().getBlockchainInfo();
+  theCall.then(
+    function (data) {
+      expect(data).to.be.not.null;
+      data.chain.should.equal('main');
+      done();
+    }
+  );  
+});
+
