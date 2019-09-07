019fc9045065acd643484d76e22ae2851121482b
Sean O'Brien <seanmobrien@hotmail.com>
2019-09-07 08:45:42 -0500

    ready to test env variables

--- src/lnpi-cli.web/routes/index.js
+++ src/lnpi-cli.web/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var config = require('../services/config/index');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -10,6 +13,8 @@ router.get('/themepad', function (req, res, next) {
   res.render('themepad', { title: 'Express' });
 });
 
-
+router.get('/config', function (req, res, next) {
+  res.send(config);
+});
 
 module.exports = router;
